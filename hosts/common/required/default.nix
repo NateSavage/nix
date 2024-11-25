a0bb3747889bdc1c9dd0c5210d70fb74369a82ba
Nate Savage <Nate.Savage@Panopticom.online>
2024-11-24 19:05:14 -0600

    Updated flake.lock

--- hosts/common/required/default.nix
+++ hosts/common/required/default.nix
@@ -1,5 +1,4 @@
-# This file (and the core directory) holds config that are used on all hosts
-# If a package is not used on *every* host, it should go in the optional directory
+# This file holds config that are used on all hosts
 {
   inputs,
   outputs,
@@ -7,25 +6,10 @@
 }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    #./acme.nix
-    #./auto-upgrade.nix
     ../apps/fish.nix
-    #./locale.nix
-    #./nix.nix
-    #./openssh.nix
-    #./optin-persistence.nix
-    #./podman.nix
-    #./sops.nix
-    #./ssh-serve-store.nix
-    #./steam-hardware.nix
-    ../apps/systemd-initrd.nix
-    #./tailscale.nix
-    #./gamemode.nix
-    #./nix-ld.nix
-    #./prometheus-node-exporter.nix
-    #./kdeconnect.nix
-    #./upower.nix
   ];
+
+  boot.initrd.systemd.enable = true;
 
   home-manager.useGlobalPkgs = true;
   home-manager.extraSpecialArgs = {
@@ -37,12 +21,6 @@
       allowUnfree = true;
     };
   };
-
-  # Fix for qt6 plugins
-  # TODO: maybe upstream this?
-  #environment.profileRelativeSessionVariables = {
-  #  QT_PLUGIN_PATH = ["/lib/qt-6/plugins"];
-  #};
 
   hardware.enableRedistributableFirmware = true;
 
