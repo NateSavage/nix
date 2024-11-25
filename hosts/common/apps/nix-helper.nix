fab3b3af75dcddf5f4bbaf82db7ffc088cd6485c
Nate Savage <Nate.Savage@Panopticom.online>
2024-11-24 20:50:31 -0600

    fix nix-helper configuration issue

--- hosts/common/apps/nix-helper.nix
+++ hosts/common/apps/nix-helper.nix
@@ -1,11 +1,5 @@
 # nh (yet another nix helper) https://github.com/viperML/nh
 { config, pkgs, ... }:
 {
-  environment.packages = [ pkgs.nh ];
-  programs.nh = {
-    enable = true;
-    #clean.enable = true;
-    #clean.extraArgs = "--keep-since 4d --keep 3";
-    #flake = "/home/user/my-nixos-config";
-  };
+  programs.nh = {  enable = true; };
 }
