02f18760ce248b28a3d9823b5c4c279552d96086
Nate Savage <Nate.Savage@panopticom.online>
2026-03-17 13:49:05 -0400

    No Sleep
    - or suspend, hibernate, you know what I mean

--- hosts/beepbox/default.nix
+++ hosts/beepbox/default.nix
@@ -19,4 +19,18 @@
 
   nixpkgs.config.allowAliases = false;
 
+  # Never sleep or suspend
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=no
+    AllowHibernation=no
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+  services.logind.extraConfig = ''
+    HandleSuspendKey=ignore
+    HandleLidSwitch=ignore
+    HandleLidSwitchExternalPower=ignore
+    IdleAction=ignore
+  '';
+
 }
